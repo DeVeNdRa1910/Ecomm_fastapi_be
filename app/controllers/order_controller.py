e13c6d28e9d869f006dcf3fcbf3f0b74b27ca4bf
Devendra <devendra.k@webkorps.com>
2026-02-05 18:40:53 +0530

    create the endpoint for the downloading the csv file of users from seller side

--- app/controllers/order_controller.py
+++ app/controllers/order_controller.py
@@ -4,6 +4,9 @@ from datetime import datetime, timezone
 from app.schemas.order_schema import (
     PaymentStatus
 )
+import csv
+import io
+from fastapi.responses import StreamingResponse
 from bson import ObjectId
 
 async def create_order_controller(order: OrderCreate, db, current_user):
@@ -56,8 +59,6 @@ async def get_orders_controller(db, current_user):
         
         {"$unwind": "$user"},
 
-        
-
        {
             "$project": {
                 "_id": 0,
@@ -100,3 +101,155 @@ async def get_orders_controller(db, current_user):
     
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Failed to get orders: {e}")
+    
+
+async def get_user_for_sellers_controllers(current_user, db):
+    # I want all the user who baught the  product 
+    pipeline = [
+        {
+            "$match": {
+                "seller_id": ObjectId(current_user["_id"])
+            }
+        },
+        
+        {
+            "$lookup": {
+                "from": "users",
+                "localField": "user_id",
+                "foreignField": "_id",
+                "as": "user"
+            }
+        },
+        
+        {"$unwind": "$user"},
+        
+        {
+            "$lookup": {
+                "from": "inventory",
+                "localField": "product_id",
+                "foreignField": "_id",
+                "as": 'product'
+            }
+        },
+        
+        {"$unwind": "$product"},
+        
+        {
+            "$project": {
+                "_id": 0,
+                "first_name": "$user.first_name",
+                "last_name": "$user.last_name",
+                "profile_image": "$user.profile_image",
+                "product_name": "$product.title",
+                "product_price": "$product.price",
+                "product_images":"$product.product_image_urls",
+                "last_ordered_time": "$created_at"
+            }
+        },
+        
+        {
+            "$sort": {
+                "last_ordered_time": -1
+            }
+        }
+    ]
+    
+    try:
+        users = await db.order.aggregate(pipeline).to_list(length=None)
+        
+        if not users:
+            return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Users are not avialables")
+        
+        return users
+    
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Failed to get users: {e}")
+    
+    
+async def export_seller_users_controller(db, current_user):
+    pipeline = [
+        {
+            "$match": {
+                "seller_id": ObjectId(current_user["_id"])
+            }
+        },
+        
+        {
+            "$lookup": {
+                "from": "users",
+                "localField": "user_id",
+                "foreignField": "_id",
+                "as": "user"
+            }
+        },
+        
+        {"$unwind": "$user"},
+        
+        {
+            "$lookup": {
+                "from": "inventory",
+                "localField": "product_id",
+                "foreignField": "_id",
+                "as": 'product'
+            }
+        },
+        
+        {"$unwind": "$product"},
+        
+        {
+            "$project": {
+                "_id": 0,
+                "first_name": "$user.first_name",
+                "last_name": "$user.last_name",
+                "profile_image": "$user.profile_image",
+                "product_name": "$product.title",
+                "product_images":"$product.product_image_urls",
+                "last_ordered_time": "$created_at"
+            }
+        },
+        
+        {
+            "$sort": {
+                "last_ordered_time": -1
+            }
+        }
+    ]
+    
+    try:
+        data = await db.order.aggregate(pipeline).to_list(length=None)
+        
+        output = io.StringIO()
+        writer = csv.writer(output)
+
+        writer.writerow([
+            "First Name",
+            "Last Name",
+            "Profile Image",
+            "Product Name",
+            "Product Images",
+            "Last Ordered Time"
+        ])
+        
+        for row in data:
+            writer.writerow([
+                row["first_name"],
+                row["last_name"],
+                row["profile_image"],
+                row["product_name"],
+                ", ".join(row["product_images"]),
+                row["last_ordered_time"]
+            ])
+
+        output.seek(0)
+        
+        return StreamingResponse(
+            output,
+            media_type="text/csv",
+            headers={
+                "Content-Disposition": "attachment; filename=seller_orders_report.csv"
+            }
+        )
+
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Failed to get users: {e}")
+    
--- app/routes/order_route.py
+++ app/routes/order_route.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, HTTPException
 from app.configs.db import get_db
 from app.services.get_current_user import get_current_user_by_token
 from app.schemas.order_schema import OrderCreate
 from app.controllers.order_controller import (
     create_order_controller,
-    get_orders_controller
+    get_orders_controller, 
+    get_user_for_sellers_controllers,
+    export_seller_users_controller,
 )
 
 router = APIRouter()
@@ -19,3 +21,15 @@ async def create_order(order: OrderCreate, db = Depends(get_db), current_user = 
 async def get_created_orders(db = Depends(get_db), current_user = Depends(get_current_user_by_token)):
     get_orders_resp = await get_orders_controller(db, current_user)
     return get_orders_resp
+
+@router.get("/get-user", status_code=status.HTTP_200_OK)
+async def get_users_for_seller(db = Depends(get_db), current_user = Depends(get_current_user_by_token)):
+    if current_user["role"] != "seller":
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You are not authorized use this endpoint")
+    get_users_resp = await get_user_for_sellers_controllers(current_user, db)
+    return get_users_resp
+
+@router.get("/seller/users/export", status_code=status.HTTP_200_OK)
+async def export_seller_users(db = Depends(get_db), current_user = Depends(get_current_user_by_token)):
+    export_seller_users_resp = await export_seller_users_controller(db, current_user)
+    return export_seller_users_resp
